a8bf3aaedd3e7f71b0688977b4e55166c30921b5
Sleepinn108 <estslavitt@gmail.com>
2024-05-16 11:45:34 -0700

    seed database products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.create(name: "product 1", price: 45, imge_url: "http://example.com/product1.jpg", description: "description for prodcut 1")
+# Product.create(name: "product 2" ), price: 50, image_url: "http://example.com/product2.jpg", description: "description for product 2"
+# Product.create(name: "product 3" ), price: 65, image_url: "http://example.com/product3.jpg", description: "description for product 3"
