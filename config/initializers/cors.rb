d9c8b290ccc42e6b24c0f0417ea252187f9b7b9b
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-08-19 13:40:13 -0700

    connted fronted end to back end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", "mine-cape-stone-frontend.onrender.com"
+    origins "example.com", "localhost:5173", "https://mine-cape-stone-frontend.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
